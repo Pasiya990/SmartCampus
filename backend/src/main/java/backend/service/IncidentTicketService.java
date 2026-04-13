e8f9295dcef4597f99068c34db4319a900347876
amamigunathilake <amamig.gunathilake@gmail.com>
2026-04-13 17:16:12 +0530

    incident ticket servide updated

--- backend/src/main/java/backend/service/IncidentTicketService.java
+++ backend/src/main/java/backend/service/IncidentTicketService.java
@@ -3,6 +3,11 @@ package backend.service;
 import backend.dto.CreateIncidentTicketRequest;
 import backend.dto.IncidentTicketResponse;
 
+import java.util.List;
+
+
 public interface IncidentTicketService {
     IncidentTicketResponse createTicket(CreateIncidentTicketRequest request);
+    List<IncidentTicketResponse> getAllTickets();
+    IncidentTicketResponse getTicketById(Long id);
 }
