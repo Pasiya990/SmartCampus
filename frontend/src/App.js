717fb695c4cf69680094db76af95cb4e379dfa98
hasangi2002 <hasini2002y@gmail.com>
2026-04-13 19:22:14 +0530

    add sample campus resources including lecture halls labs meeting rooms and equipment for testing and demonstration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,38 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   const [message, setMessage] = useState("Connecting...");
+
+//   useEffect(() => {
+//     fetch(process.env.REACT_APP_API_URL + "/test")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data);
+
+//         if (data.length >= 0) {
+//           setMessage("PostgresSQL Connected Successfully ✅");
+//         }
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         setMessage("Connection Failed ❌");
+//       });
+//   }, []);
+
+//   return (
+//     <div style={{ textAlign: "center", marginTop: "50px" }}>
+//       <h1>SmartCampus</h1>
+//       <h2>{message}</h2>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import ResourceCatalogue from "./pages/ResourceCatalogue";
 
 function App() {
-  const [message, setMessage] = useState("Connecting...");
-
-  useEffect(() => {
-    fetch(process.env.REACT_APP_API_URL + "/test")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-
-        if (data.length >= 0) {
-          setMessage("PostgresSQL Connected Successfully ✅");
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        setMessage("Connection Failed ❌");
-      });
-  }, []);
-
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>SmartCampus</h1>
-      <h2>{message}</h2>
-    </div>
-  );
+  return <ResourceCatalogue />;
 }
 
 export default App;
