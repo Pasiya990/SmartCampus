80a49f8cf53a471fa336786b740f32b2e6f645db
hasangi2002 <hasini2002y@gmail.com>
2026-04-19 11:36:32 +0530

    Add AnalyticsAdminDash peak hour DTO

---
+++ backend/src/main/java/backend/dto/AnalyticsAdminDashPeakHourDTO.java
@@ -0,0 +1,17 @@
+package backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AnalyticsAdminDashPeakHourDTO {
+
+    private Integer hour;
+    private String label;
+    private long bookingCount;
+}
