d7a6ad5eb7d1396433004e33fc17cd83221466a0
hasangi2002 <hasini2002y@gmail.com>
2026-04-19 11:32:57 +0530

    Add AnalyticsAdminDash summary DTO

---
+++ backend/src/main/java/backend/dto/AnalyticsAdminDashSummaryDTO.java
@@ -0,0 +1,27 @@
+package backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AnalyticsAdminDashSummaryDTO {
+
+    private long totalResources;
+    private long activeResources;
+    private long outOfServiceResources;
+
+    private long totalBookings;
+    private long approvedBookings;
+    private long pendingBookings;
+    private long rejectedBookings;
+    private long cancelledBookings;
+
+    private long utilizedResources;
+    private double approvalRate;
+    private double averageApprovedBookingHours;
+}
